2bd00072c5c7073819659ef0d97843d3e7c6bf88
[Steve Wallace] <[swallace35@gmail.com]>
2017-03-20 11:34:34 -0400

    Reverse String Algorithm

---
+++ ReverseString.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function reverseString(str) {
+  var o = " ";
+  for (var i = str.length - 1; i >= 0; i--) {
+    o += str[i];
+  }
+  return o;
+}
+
+reverseString("Howdy");
